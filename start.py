f1b1cd2fb2ec8717c421651190f2f1427d668569
AndersonOyama <anderson.oyama@hotmail.com>
2020-03-24 23:28:53 -0300

    Fails

--- start.py
+++ start.py
@@ -1,25 +1,22 @@
-import sys
+import re
 
 from spellchecker import SpellChecker
 
-def main():
+def countSpellError(file):
 
     spell = SpellChecker(language='pt')
-
-    with open('./Noticias/Falsas/Banho frio e desmaios.txt') as f:
-        lines = f.read().splitlines()
-
-
-
-
+    lines = []
+    with open(file, 'r') as f:
+        lines = [word.replace('.', '').replace(',','') for line in f for word in line.split()]
+    f.close()
+    count = 0
     misspelled = spell.unknown(lines)
 
     for word in misspelled:
-        spell.correction(word)
-        print(word)
-
-    pass
-
-
-if __name__ == "__main__":
-    main()
+        # spell.correction(word)
+        # print(word)
+        # print(word, spell.correction(word))
+        count += 1
+    
+    
+    return misspelled, count
